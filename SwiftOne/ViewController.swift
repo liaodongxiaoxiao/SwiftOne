3cf7525173044aa6883983c58cfd207668ebda67
liaodongxiaoxiao <liaodongxiaoxiao@gmail.com>
2016-02-17 22:17:48 +0800

    Initial Commit

---
+++ SwiftOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftOne
+//
+//  Created by ldxx on 16/2/17.
+//  Copyright © 2016年 ldxx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
